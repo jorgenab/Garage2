e1cfac9a3512755fe947d162ee26f21d42cfd9e0
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-05-10 21:31:49 +0200

    Created ParkedVehicle model

---
+++ Garage2/Models/ParkedVehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int NumberOfWheels { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateTime TimeOfParking { get; set; }
+    }
+
+    public enum VehicleType
+    {
+        Car,
+        Boat,
+        MotorCycle,
+        Buss,
+        Airplane,
+        Cycle
+    }
+}
