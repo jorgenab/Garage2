d2f96e4a8326bd18824f1fa7782dff414ab088ca
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-05-11 13:24:39 +0200

    Added ParkedVehicle model and created ParkedVehiclesController by chosing "MVC Controller with views using Entity Framework".

---
+++ Garage2/Migrations/20200510194845_init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage2.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParkedVehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleType = table.Column<int>(nullable: false),
+                    RegistrationNumber = table.Column<string>(nullable: true),
+                    NumberOfWheels = table.Column<int>(nullable: false),
+                    Color = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    TimeOfParking = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParkedVehicle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParkedVehicle");
+        }
+    }
+}
